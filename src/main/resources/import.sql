479ec04659fc7fb7df970d3695b4d61ed5628110
Benjamin, Binghui Zhong <zhongbinghui1995@gmail.com>
2021-08-18 00:29:35 +0800

    try new application.properties setting

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO balance(id, first_name, last_name, balance)VALUES(12345678, 'Mary', 'Chan', 1000000);
+INSERT INTO balance(id, first_name, last_name, balance)VALUES(88888888, 'Peter', 'Li', 1000000);
