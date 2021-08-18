dc2c8a6ecd4d5eca5c4571782da171c45ec0c136
Benjamin, Binghui Zhong <zhongbinghui1995@gmail.com>
2021-08-18 22:27:50 +0800

    update application.properties for support data persist

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO balance(id, first_name, last_name, balance)
+INSERT IGNORE INTO balance(id, first_name, last_name, balance)
 VALUES(12345678, 'Mary', 'Chan', 1000000),
 (88888888, 'Peter', 'Li', 1000000);
