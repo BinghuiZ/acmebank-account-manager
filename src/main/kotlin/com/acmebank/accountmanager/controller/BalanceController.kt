6075b3c7844456a20915d2b2a444943957f69591
Benjamin, Binghui Zhong <zhongbinghui1995@gmail.com>
2021-08-18 22:26:41 +0800

    add cusResponseObject for API response

--- src/main/kotlin/com/acmebank/accountmanager/controller/BalanceController.kt
+++ src/main/kotlin/com/acmebank/accountmanager/controller/BalanceController.kt
@@ -14,37 +14,39 @@ import org.springframework.web.bind.annotation.*
 class BalanceController(@Autowired val balanceService: BalanceService) {
 
     @GetMapping("/{id}")
-    fun getBalanceById(@PathVariable("id") id: Int): ResponseEntity<Any> {
+    fun getBalanceById(@PathVariable("id") id: Int): ResponseEntity<CusResponseObject> {
         try {
             val balance = balanceService.getBalance(id)
-            return ResponseEntity<Balance>(balance, HttpStatus.OK) as ResponseEntity<Any>
+            return ResponseEntity<Balance>(balance, HttpStatus.OK) as ResponseEntity<CusResponseObject>
         } catch (e: BalanceNotFoundException) {
-            return ResponseEntity<ErrorHandleObject>(ErrorHandleObject(e.message), HttpStatus.BAD_REQUEST) as ResponseEntity<Any>
+            return ResponseEntity<ErrorHandleObject>(ErrorHandleObject(e.message), HttpStatus.BAD_REQUEST) as ResponseEntity<CusResponseObject>
         }
     }
 
     @GetMapping("/list")
-    fun getAllBalance(): ResponseEntity<Any> {
+    fun getAllBalance(): ResponseEntity<CusResponseObject> {
         val list = balanceService.getAllBalance()
         if (list.isEmpty()) {
-            return ResponseEntity<ErrorHandleObject>(ErrorHandleObject("No Records Found."), HttpStatus.BAD_REQUEST) as ResponseEntity<Any>
+            return ResponseEntity<ErrorHandleObject>(ErrorHandleObject("No Records Found."), HttpStatus.BAD_REQUEST) as ResponseEntity<CusResponseObject>
         }
-        return ResponseEntity<List<Balance>>(list, HttpStatus.OK) as ResponseEntity<Any>
+        return ResponseEntity<List<Balance>>(list, HttpStatus.OK) as ResponseEntity<CusResponseObject>
     }
 
     @PostMapping("/transfer")
     @ResponseBody
     fun transferMoney(@RequestParam("from") from: Int,
                       @RequestParam("to") to: Int,
-                      @RequestParam("amount") amount: Double): ResponseEntity<Any> {
+                      @RequestParam("amount") amount: Double): ResponseEntity<CusResponseObject> {
         try {
-            return ResponseEntity<Balance>(balanceService.transferMoney(from, to, amount), HttpStatus.OK) as ResponseEntity<Any>
+            return ResponseEntity<Balance>(balanceService.transferMoney(from, to, amount), HttpStatus.OK) as ResponseEntity<CusResponseObject>
         } catch (e: BalanceNotFoundException) {
-            return ResponseEntity<ErrorHandleObject>(ErrorHandleObject(e.message), HttpStatus.BAD_REQUEST) as ResponseEntity<Any>
+            return ResponseEntity<ErrorHandleObject>(ErrorHandleObject(e.message), HttpStatus.BAD_REQUEST) as ResponseEntity<CusResponseObject>
         } catch (e: InsufficientBalanceException) {
-            return ResponseEntity<ErrorHandleObject>(ErrorHandleObject(e.message), HttpStatus.BAD_REQUEST) as ResponseEntity<Any>
+            return ResponseEntity<ErrorHandleObject>(ErrorHandleObject(e.message), HttpStatus.BAD_REQUEST) as ResponseEntity<CusResponseObject>
         }
     }
 }
 
-data class ErrorHandleObject(val error: String? = "")
+interface CusResponseObject{}
+
+data class ErrorHandleObject(val error: String? = ""): CusResponseObject
--- src/main/kotlin/com/acmebank/accountmanager/model/Balance.kt
+++ src/main/kotlin/com/acmebank/accountmanager/model/Balance.kt
@@ -1,12 +1,13 @@
 package com.acmebank.accountmanager.model
 
+import com.acmebank.accountmanager.controller.CusResponseObject
 import javax.persistence.Entity
 import javax.persistence.Id
 import javax.persistence.Table
 
 @Entity
 @Table(name="balance")
-class Balance {
+class Balance: CusResponseObject {
     @Id var id: Int = 0
     var first_name: String = ""
     var last_name: String = ""
